94624991df5a8563dc920d9ee430c02bd9c2ae8b
Achintya Kumar Debnath <debnathachintya@gmail.com>
2016-07-27 17:01:15 +0530

    Part of NetTest1.java Test Case

---
+++ src/Hello/NewTest2.java
@@ -0,0 +1,11 @@
+package Hello;
+
+
+
+public class NewTest2 {
+	public static final String baseURL = "http://www.demo.guru99.com/V4/index.php";
+	
+	public static final String opFile = "C:/Selenium/myScreen.jpg";
+	
+	
+}
